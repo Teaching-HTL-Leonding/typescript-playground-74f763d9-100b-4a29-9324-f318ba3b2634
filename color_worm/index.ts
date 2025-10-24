b7519334654119d9704e44aedba41cee9036ba10
Rainer Stropek <rainer@timecockpit.com>
2025-10-24 08:46:07 +0200

    Saved in TypeScript Playground at 2025-10-24T06:46:07.198Z

---
+++ color_worm/index.ts
@@ -0,0 +1,21 @@
+function setup() {
+    createCanvas(500,500);
+    colorMode(HSB);
+    background(0, 100, 0);
+    noStroke();
+
+}
+
+
+//Globale Variable
+let color_hue: number = 0; 
+
+function mouseMoved() {
+    fill(color_hue, 100, 100);
+    circle(mouseX, mouseY, 40);
+    color_hue = color_hue + 5;
+    color_hue = color_hue % 360;
+    //console.log(color_hue);
+
+
+}
