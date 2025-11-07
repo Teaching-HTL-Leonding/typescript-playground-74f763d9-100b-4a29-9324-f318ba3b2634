1f6e8e361728f00c79ffb8dc01fb456a9125cca3
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:30:29 +0100

    Saved in TypeScript Playground at 2025-11-07T07:30:29.117Z

--- rect_tool_exam/index.ts
+++ rect_tool_exam/index.ts
@@ -11,7 +11,23 @@ function mouseMoved () {
     strokeWeight(2);
     noFill();
 
-    let diameterX = Math.ads((mouseX - ))
+    let diameterX = Math.abs((mouseX - width / 2) * 2);
+    diameterX = Math.round ( mouseX / gridSize * 2);
+    let diameterY = Math.abs ((mouseY - height / 2) * 2)
+    diameterX = Math.round (mouseY / gridSize * 2)
+
+    stroke("green");
+    arc(width/2, height/2, diameterX, diameterY, 0, 90);
+    stroke("pink");
+    arc(width/2, height/2, diameterX, diameterY, 90, 180);
+    stroke("gray");
+    arc(width/2, height/2, diameterX, diameterY, 180, 270);
+    stroke("red");
+    arc(width/2, height/2, diameterX, diameterY, 270, 360);
+    
+    
+
+
 
 
 
