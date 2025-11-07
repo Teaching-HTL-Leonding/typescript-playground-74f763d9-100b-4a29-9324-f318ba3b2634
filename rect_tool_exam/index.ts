adf6f48fd84e4fe07f0c70165c8fa8d11055b161
Rainer Stropek <rainer@timecockpit.com>
2025-11-07 08:23:13 +0100

    Saved in TypeScript Playground at 2025-11-07T07:23:12.855Z

---
+++ rect_tool_exam/index.ts
@@ -0,0 +1,34 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    angleMode(DEGREES);
+}
+
+function mouseMoved () {
+    const gridSize = 20;
+    
+    background("black");
+    strokeWeight(2);
+    noFill();
+
+    let diameterX = Math.ads((mouseX - ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
