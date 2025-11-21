c47a5b712a090ef1bf2df52f553a4cdf7fb51fa0
Rainer Stropek <rainer@timecockpit.com>
2025-11-21 08:58:48 +0100

    Saved in TypeScript Playground at 2025-11-21T07:58:48.456Z

---
+++ color_quadrants/index.ts
@@ -0,0 +1,36 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    noStroke();
+
+    fill("orange");
+    rect(0, 0, width / 2, height / 2);
+
+    fill("red");
+    rect(width / 2, 0, width / 2, height / 2);
+
+    fill("green");
+    rect(width / 2, height / 2, width / 2, height /2);
+
+    fill("yellow");
+    rect( 0, height / 2, width / 2, height / 2);
+
+    fill("black");
+    rect( width / 4 , height / 2 - height / 4 / 2, width / 2, height / 4); 
+    
+}
+
+function moveClicked () {
+
+
+
+
+
+
+
+
+
+
+
+
+}
