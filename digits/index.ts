f7a85a939e83adf7126fa09fd0ec906868140713
Rainer Stropek <rainer@timecockpit.com>
2025-11-28 09:33:31 +0100

    Saved in TypeScript Playground at 2025-11-28T08:33:31.159Z

---
+++ digits/index.ts
@@ -0,0 +1,41 @@
+
+function setup() {
+  const num = Math.floor(random(0, 1_000_000));
+
+  createCanvas(500,300);
+  background("black");
+  
+  textAlign(CENTER, CENTER);
+  fill("yellow");
+  noStroke();
+  text(num, width / 2, height - 20);
+
+  noFill();
+  stroke("yellow");
+  rect(40, 100, 50, 90);
+  rect(110, 100, 50, 90);
+  rect(180, 100, 50, 90);
+  rect(250, 100, 50, 90);
+  rect(320, 100, 50, 90);
+  rect (390, 100, 50, 90);
+
+  const digit1 = num%10
+  const digit2 = floor(num / 10) % 10
+  const digit3 = floor(num / 100) % 10 
+  const digit4 = floor(num / 1000) % 10
+  const digit5 = floor(num / 10000) % 10
+  const digit6 = floor(num / 100000) % 10
+
+  fill("yellow");
+  textSize(40);
+  text(digit6, width / 7.5, height / 2);
+  text(digit5, width / 3.7, height / 2);
+  text(digit4, width / 2.4, height / 2);
+  text(digit3, width / 1.8, height / 2);
+  text(digit2, width / 1.45, height /2);
+  text(digit1, width / 1.2, height / 2);
+
+
+  
+
+}
