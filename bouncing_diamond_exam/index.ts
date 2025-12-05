d313ca99d13ef3174bb1acbfe065db172d1e5688
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 08:49:38 +0100

    Saved in TypeScript Playground at 2025-12-05T07:49:37.731Z

---
+++ bouncing_diamond_exam/index.ts
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(300, 300);
+}
+
+
+
+function draw() {
+    background("gold");
+
+    line(20, 30, 40, 50);
+    line(20, 30, 40, 10);
+    line(60, 30, 40, 10);
+    line(60, 30, 40, 50);
+
+
+    
+
+}   
