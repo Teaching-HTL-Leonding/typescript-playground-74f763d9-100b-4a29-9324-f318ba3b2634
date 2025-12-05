06e2bde6949a51d01cf7ecd9dd78dbba4d7ae436
Rainer Stropek <rainer@timecockpit.com>
2025-12-05 09:40:47 +0100

    Saved in TypeScript Playground at 2025-12-05T08:40:47.389Z

--- bouncing_diamond_exam/index.ts
+++ bouncing_diamond_exam/index.ts
@@ -1,18 +1,62 @@
 function setup() {
     createCanvas(300, 300);
+    textAlign(CENTER, CENTER);
+    textSize(24);
+
 }
+
+let x = 50
+let y = 50
+let size = 10
+let speedX = 2
+let speedY = 2
+let counter = 5
+
+let circleCenterX: number;
+let circleCenterY: number;
+
+
+let directionX = 2;
+let directionY = 2;
+
+
+
+
+
 
 
 
 function draw() {
     background("gold");
 
-    line(20, 30, 40, 50);
-    line(20, 30, 40, 10);
-    line(60, 30, 40, 10);
-    line(60, 30, 40, 50);
+    stroke("black");
+
+    line(x - 20, y, x, y + 20);
+    line(x - 20, y, x, y - 20);
+    line(x + 20, y, x, y - 20);
+    line(x + 20, y, x, y + 20);
+
+    noStroke()
+    x += speedX
+    y += speedY
+
+    circleCenterX += directionX;
+    circleCenterY += directionY;
+
+    if (circleCenterX <= 0|| circleCenterX >= width) {
+        directionX *= -1;
+    }
+    if (circleCenterY <= 0 || circleCenterY >= height) {
+        directionY *= -1; 
+    }
+}
+
+
+
+
+
 
 
     
 
-}   
+
