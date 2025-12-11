e0e907b8f2eded20216485817d1d1732d77b0c31
Rainer Stropek <rainer@timecockpit.com>
2025-12-11 21:14:07 +0100

    Saved in TypeScript Playground at 2025-12-11T20:14:06.643Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -2,18 +2,18 @@ function setup() {
     createCanvas(500, 500);
     background("red");
 
-    let i = 1;
-    while (i < 10) {
+  
+    
+    for (let i = 1; i < 6; i += 1) {
         fill("black");
-        circle(50 * i, 50, 50);
-        i += 1;
-    }
-
-    for (let j = 1; j < 10; j += 1) {
-        fill("black");
-        circle(50 * j, 200, 50);
+        rect(50 * i, 40, 40);
     }
 }
+
+    
+
+
+
 
 
 
