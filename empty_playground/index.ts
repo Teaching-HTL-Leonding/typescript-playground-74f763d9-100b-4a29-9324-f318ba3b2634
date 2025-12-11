aa030990aeb537480ffc6cb999983d22dfdab4ef
Rainer Stropek <rainer@timecockpit.com>
2025-12-11 13:20:50 +0100

    Saved in TypeScript Playground at 2025-12-11T12:20:50.104Z

--- empty_playground/index.ts
+++ empty_playground/index.ts
@@ -2,11 +2,20 @@ function setup() {
     createCanvas(500, 500);
     background("red");
 
-    let i = 0;
-    while (i < 11) {
+    let i = 1;
+    while (i < 10) {
         fill("black");
         circle(50 * i, 50, 50);
         i += 1;
     }
+
+    for (let j = 1; j < 10; j += 1) {
+        fill("black");
+        circle(50 * j, 200, 50);
+    }
 }
 
+
+
+// abweisende Schleife 
+// NICHT abweisende Schleife 
