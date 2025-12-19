b1efa6a0b746f5efd9d7f5b057c730df71ab964a
Rainer Stropek <rainer@timecockpit.com>
2025-12-19 08:55:51 +0100

    Saved in TypeScript Playground at 2025-12-19T07:55:50.978Z

---
+++ zebra_stripes/index.ts
@@ -0,0 +1,33 @@
+function setup() {
+    const SIZE = 400;  // Size of canvas
+    const STRIPE_THICKNESS = 50; // Thickness of the stripes
+
+    createCanvas(SIZE, SIZE);
+    background("lime");
+
+    noStroke();
+
+
+    let x = 1;
+    while (x < 8) {  
+
+        fill("yellow");
+        noStroke();
+        rect(0, x * STRIPE_THICKNESS, width, STRIPE_THICKNESS);
+        x+=2
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
